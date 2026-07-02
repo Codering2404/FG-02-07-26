218edd692a509e3a78ee9adaefd313954131203a
Codering2404 <menhubbusiness@gmail.com>
2026-07-02 11:52:38 -0600

    Add files via upload

--- R.java
+++ R.java
@@ -1,7 +1,16 @@
-package androidx.compose.foundation.layout;
+package androidx.compose.foundation;
 
 /* JADX INFO: loaded from: classes2.dex */
 public final class R {
+
+    public static final class string {
+        public static int tooltip_description = 0x7f0b0080;
+        public static int tooltip_label = 0x7f0b0081;
+
+        private string() {
+        }
+    }
+
     private R() {
     }
 }
